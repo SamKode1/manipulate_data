800b28c0c3f84cc63dec8b7235f59d924f522d24
Samkode <samu92dls@gmail.com>
2023-03-16 06:42:14 -0300

    adding directory 'module' and first module transform data

---
+++ module/data_to_csv.py
@@ -0,0 +1,17 @@
+import csv 
+
+with open("/home/samkodde/curso_comprehension_python/final_challenge/world_population.csv", "r+") as data:
+   reader = csv.reader(data, delimiter=",") # We process data in a csv format, using the parameter delimiter "," as a separator between data and data
+   header = next(reader) # using iteratation next() we only reproduce 1 row of the data  and capture it as a header row
+   #print(header)
+   final_data = [] # then create a epty list waiting for assign the organized data
+
+
+# using zip() we can match the first item (header[0]) to the second item (reader[0]), to match in pair key: value with the metod dict().
+#  Finally using append we add all data into a epty list final data
+
+   for row in reader:
+      in_pairs = zip(header,row)
+      convert_dict = dict(in_pairs)
+      final_data.append(convert_dict)
+      print(final_data)
